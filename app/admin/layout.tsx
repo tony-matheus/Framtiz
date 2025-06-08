5645d8dea84ba2a908d2295483d99aa324b0b06a
Tony <mantony0148@icloud.com>
2025-06-08 11:44:20 -0500

    Remove old sidebar and uprank the experimental sidebar

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 
 import { serverAuthService } from '@/lib/services/auth/server-auth-service';
-import AdminLayout from '@/components/ui-custom/experiments/practical-sidebar/admin-layout';
+import AdminLayout from '@/components/admin/admin-layout/admin-layout';
 
 export default async function Layout({ children }: { children: ReactNode }) {
   const user = await serverAuthService.getCurrentUser();
